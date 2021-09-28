ebdc48df48f37d5c0d230eb0521d7765b0bab74b
kento sugimoto <kento1109@gmail.com>
2021-09-28 19:49:11 +0900

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBW9cD0Tgpm3UTTx4f5rBZ5Z7XD0tu2sP4",
-  authDomain: "drug-manager-82b2f.firebaseapp.com",
-  projectId: "drug-manager-82b2f",
-  storageBucket: "drug-manager-82b2f.appspot.com",
-  messagingSenderId: "630683641471",
-  appId: "1:630683641471:web:d6979b37b7e3c96e99c14a"
 };
 
 // Initialize Firebase
